bae1745af99c8d59592336db66d4cb7e128ba641
HerbertKayky <herbertkayky9@gmail.com>
2024-10-09 00:00:23 -0300

    componentizando Footer e ContactSection

---
+++ src/app/components/ContactSection/index.tsx
@@ -0,0 +1,33 @@
+// ContactSection.js
+import Image from "next/image";
+import Link from "next/link";
+import wppIcon from "/public/wppIcon.png";
+
+export default function ContactSection() {
+  return (
+    <section id="contact" className="py-16 bg-[#C53030] text-white">
+      <div className="container mx-auto px-6 text-center">
+        <h3 className="text-3xl font-bold">Entre em Contato</h3>
+        <p className="mt-4 text-lg">
+          Quer saber mais sobre algum modelo ou agendar um test ride? <br />{" "}
+          Fale diretamente comigo agora.
+        </p>
+        <div className="inline-flex items-center px-6 py-2 rounded-lg bg-green-600 gap-3 mt-10 transition-transform hover:scale-105">
+          <Image
+            src={wppIcon}
+            alt="WhatsApp"
+            width={40}
+            priority
+            quality={100}
+          />
+          <Link
+            href="https://wa.me/SEU_NUMERO"
+            className="text-quase text-xl font-medium"
+          >
+            Entre em contato
+          </Link>
+        </div>
+      </div>
+    </section>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,16 +7,15 @@ import Header from "./components/Header";
 import HeroSection from "./components/HeroSection";
 import AboutSection from "./components/AboutSection";
 import Testimonials from "./components/Testimonials";
+import Footer from "./components/Footer";
+import ContactSection from "./components/ContactSection";
 
 export default function Motorac() {
   return (
     <div className="bg-gray-100 min-h-screen">
       <Header />
-
       <HeroSection />
-
       <AboutSection />
-
       <Testimonials />
 
       {/* Models Section */}
@@ -126,38 +125,8 @@ export default function Motorac() {
         </div>
       </section>
 
-      {/* Contact Section */}
-      <section id="contact" className="py-16 bg-[#C53030] text-white">
-        <div className="container mx-auto px-6 text-center">
-          <h3 className="text-3xl font-bold">Entre em Contato</h3>
-          <p className="mt-4 text-lg">
-            Quer saber mais sobre algum modelo ou agendar um test ride? Fale
-            diretamente comigo agora.
-          </p>
-          <div className="inline-flex items-center px-6 py-2 rounded-lg bg-green-600 gap-3 mt-10 transition-transform hover:scale-105">
-            <Image
-              src={wppIcon}
-              alt="WhatsApp"
-              width={40}
-              priority={true}
-              quality={100}
-            />
-            <Link
-              className="text-quase text-xl font-medium"
-              href="https://wa.me/SEU_NUMERO"
-            >
-              Entre em contato
-            </Link>
-          </div>
-        </div>
-      </section>
-
-      {/* Footer */}
-      <footer className="bg-gray-900 text-gray-400 py-6">
-        <div className="container mx-auto px-6 text-center">
-          <p>&copy; 2024 Herbert Kayky - Todos os direitos reservados.</p>
-        </div>
-      </footer>
+      <ContactSection />
+      <Footer />
     </div>
   );
 }
